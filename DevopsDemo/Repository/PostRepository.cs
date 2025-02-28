7ad7bd85d0cd8508ef58d3488000b766362ccea9
infinityloki <107397846+infinityloki@users.noreply.github.com>
2025-02-28 18:05:58 +0530

    Add project files.

---
+++ DevopsDemo/Repository/PostRepository.cs
@@ -0,0 +1,17 @@
+﻿using DevOpsDemo.Models;
+using System.Collections.Generic;
+namespace DevOpsDemo.Repository
+{
+    public class PostRepository : IPostRepository
+    {
+        public List<PostViewModel> GetPosts()
+        {
+            var posts = new List<PostViewModel> {
+ new PostViewModel(){ PostId =101, Title = "DevOps Demo Title 1", Description ="DevOps DemoDescription 1", Author="raju"},
+ new PostViewModel(){ PostId =102, Title = "DevOps Demo Title 2", Description ="DevOps DemoDescription 2", Author="mukesh"},
+ new PostViewModel(){ PostId =103, Title = "DevOps Demo Title 3", Description ="DevOps DemoDescription 3", Author="brother"},
+ };
+            return posts;
+        }
+    }
+}
